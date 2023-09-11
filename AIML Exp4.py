11615b2eb612d9900cced7391b6a0a08ab7969f4
Gautam Kumar <gk1497singh@gmail.com>
2023-09-11 12:23:25 +0530

    AIML Exp4

--- AIML Exp4.py
+++ AIML Exp4.py
@@ -18,8 +18,13 @@ outcome_counts = data['Outcome'].value_counts()
 
 # Create a bar plot of mission outcomes with custom labels and title
 plt.figure(figsize=(10, 6))
-plt.bar(outcome_counts.index, outcome_counts.values)
+bars=plt.bar(outcome_counts.index, outcome_counts.values)
 plt.xlabel('Outcome')  # Label for the x-axis
 plt.ylabel('Count')    # Label for the y-axis
 plt.title('Mission Outcomes')  # Title for the plot
+
+# Annotate the bars with their counts
+for bar, count in zip(bars, outcome_counts.values):
+    plt.text(bar.get_x() + bar.get_width()/2, bar.get_height(), str(count), ha='center', va='bottom')
+
 plt.show()  # Display the plot
